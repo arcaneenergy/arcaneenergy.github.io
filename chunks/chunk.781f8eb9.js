d6000ace120b4e3cce226fe5361b802a96ae95b9
arcaneenergy <52855634+arcaneenergy@users.noreply.github.com>
2023-02-07 20:02:01 +0000

    Update

---
+++ chunks/chunk.781f8eb9.js
@@ -0,0 +1,20 @@
+import './chunk.67c838a9.js';
+import 'html-escaper';
+
+const id = "multimesh-scatter.mdx";
+const collection = "posts";
+const slug = "multimesh-scatter";
+const body = '\nimport Youtube from "@components/Youtube.astro";\nimport GithubButton from "@components/GithubButton.astro";\n\n<GithubButton embedId="godot-multimesh-scatter" />\n\n![Image 1](/posts/multimesh-scatter/random-rotation.webp)\n\n---\n\n## Download\n\n[MultiMesh Scatter](https://github.com/arcaneenergy/godot-multimesh-scatter)\n\n## How to use\n\n1. Download this [repository](https://github.com/arcaneenergy/godot-multimesh-scatter) or download the addon from the\n   AsseLib in Godot.\n2. Import the addons folder into your project.\n3. Activate MultiMesh Scatter under Project > Project Settings > Plugins.\n4. Add a MultiMeshScatter node to the scene.\n\n<video width="320" height="240" controls>\n  <source\n    src="https://user-images.githubusercontent.com/52855634/213530097-8c653666-5b24-41b0-91d2-4bb9b68750b7.mp4"\n    type="video/mp4"\n  />\n</video>\n\n<video width="320" height="240" controls>\n  <source\n    src="https://user-images.githubusercontent.com/52855634/213530108-fa75e5d3-0d4c-49f6-a449-f4df0d07f92c.mp4"\n    type="video/mp4"\n  />\n</video>\n\n<video width="320" height="240" controls>\n  <source\n    src="https://user-images.githubusercontent.com/52855634/213530112-e70b88b1-933b-407d-b491-c99db91d9ee1.mp4"\n    type="video/mp4"\n  />\n</video>\n\n## Features\n\n- Scatter objects in the scene using a MultiMeshInstance node.\n- Adjust the instance count, size, and collision layer.\n- Randomize size and rotation of each instance.\n- Automatically rotates each instance to the normal of the terrain.\n\n## Parameters\n\n- `count`: The number of instances to generate.\n- `placement_type`: Defines the placement type.\n- `placement_size`: The placement size of the bounding box. Enable `show_debug_area` to view the size of the bounding\n  box. Note: If the `placement_type` is set to Sphere, only the x value will be used to specify the radius of the\n  sphere.\n- `collision_mask`: The physics collision mask that the instances should collide with.\n- `offset_position`: Add an offset to the placed instances.\n- `offset_rotation`: Add a rotation offset to the placed instances.\n- `base_scale`: Change the base scale of the instanced meshes.\n- `min_random_size`: The minimum random size for each instance.\n- `max_random_size`: The maximum random size for each instance.\n- `random_rotation`: Rotate each instance by a random amount between. `-random_rotation` and `+random_rotation`.\n- `seed`: A seed to feed for the random number generator.\n- `show_debug_area`: Toggle the visibility of the bounding box area.\n\n## Notes\n\n- The sphere placement type takes `placement_size.x` for the radius. The y and z values are not used.\n- The sphere placement type behaves more like a capsule shape. This means that only the horizontal radius is taken\n  into account when scattering meshes.\n- Scattering occurs automatically in the editor whenever you change a parameter or move the MultiMeshScatter node.\n  In game mode, the scatter occurs once at the beginning of the game.\n\n## License\n\n[MIT License](/LICENSE.md)\n\n<Youtube videoId="2fa4guxq6iU" />\n';
+const data = {
+  title: "MultiMesh Scatter",
+  description: "A simple tool to randomly place meshes.",
+  datetime: new Date(16698528e5),
+  heroImage: "/posts/multimesh-scatter/banner.webp",
+  tags: ["godot", "open-source", "addon"]
+};
+const _internal = {
+  filePath: "/home/runner/work/website/website/src/content/posts/multimesh-scatter.mdx",
+  rawData: '\nslug: multimesh-scatter\ntitle: MultiMesh Scatter\ndescription: A simple tool to randomly place meshes.\ndatetime: 2022-12-01\nheroImage: "/posts/multimesh-scatter/banner.webp"\ntags:\n  - godot\n  - open-source\n  - addon'
+};
+
+export { _internal, body, collection, data, id, slug };
---
+++ chunks/chunk.be44e47a.js
@@ -0,0 +1,166 @@
+import { _ as __astro_tag_component__, F as Fragment, j as createVNode } from './chunk.67c838a9.js';
+import { $ as $$Youtube } from './chunk.e774dd31.js';
+import { $ as $$ItchioWidget } from './chunk.5610b14e.js';
+import 'html-escaper';
+/* empty css                *//* empty css                */
+const frontmatter = {
+  "slug": "divided-chains-combined-brains",
+  "title": "Divided Chains Combined Brains",
+  "description": "Control one or multiple chunks and complete puzzles.",
+  "datetime": "2021-06-12T00:00:00.000Z",
+  "heroImage": "/posts/divided-chains-combined-brains/banner.gif",
+  "tags": ["godot", "itch-io", "game"]
+};
+function getHeadings() {
+  return [{
+    "depth": 2,
+    "slug": "trailer",
+    "text": "Trailer"
+  }, {
+    "depth": 2,
+    "slug": "screenshots",
+    "text": "Screenshots"
+  }, {
+    "depth": 2,
+    "slug": "description",
+    "text": "Description"
+  }, {
+    "depth": 3,
+    "slug": "controls",
+    "text": "Controls"
+  }];
+}
+function _createMdxContent(props) {
+  const _components = Object.assign({
+    p: "p",
+    a: "a",
+    strong: "strong",
+    h2: "h2",
+    img: "img",
+    h3: "h3",
+    pre: "pre",
+    code: "code",
+    span: "span"
+  }, props.components);
+  return createVNode(Fragment, {
+    children: [createVNode($$ItchioWidget, {
+      embedId: "1080661"
+    }), "\n", createVNode(_components.p, {
+      children: ["Submitted to the ", createVNode(_components.a, {
+        href: "https://itch.io/jam/gmtk-2021",
+        children: "GMTK Game Jam 2021"
+      }), " - 48 hour game making marathon, focused on\ndesign, mechanics, and clever ideas. From June 11th, at 7PM UK time, to June 13th, at 7PM UK time. The theme: ", createVNode(_components.strong, {
+        children: "Joined\nTogether"
+      }), "."]
+    }), "\n", createVNode(_components.p, {
+      children: [createVNode(_components.a, {
+        href: "https://itch.io/jam/gmtk-2021/rate/1080661",
+        children: "View the submission"
+      }), "."]
+    }), "\n", createVNode(_components.h2, {
+      id: "trailer",
+      children: "Trailer"
+    }), "\n", createVNode($$Youtube, {
+      videoId: "I66_MyAeb4I"
+    }), "\n", createVNode(_components.h2, {
+      id: "screenshots",
+      children: "Screenshots"
+    }), "\n", createVNode(_components.p, {
+      children: [createVNode(_components.img, {
+        src: "/posts/divided-chains-combined-brains/image-01.webp",
+        alt: "Image 1"
+      }), "\n", createVNode(_components.img, {
+        src: "/posts/divided-chains-combined-brains/image-02.webp",
+        alt: "Image 2"
+      }), "\n", createVNode(_components.img, {
+        src: "/posts/divided-chains-combined-brains/image-03.webp",
+        alt: "Image 3"
+      }), "\n", createVNode(_components.img, {
+        src: "/posts/divided-chains-combined-brains/image-04.webp",
+        alt: "Image 4"
+      })]
+    }), "\n", createVNode(_components.h2, {
+      id: "description",
+      children: "Description"
+    }), "\n", createVNode(_components.p, {
+      children: "Every chunk shares the same controls. That means each chunk moves\nin the same direction as the other chunks."
+    }), "\n", createVNode(_components.p, {
+      children: "Play in 6 unique levels. Each level has a unique atmosphere and different backgrounds."
+    }), "\n", createVNode(_components.h3, {
+      id: "controls",
+      children: "Controls"
+    }), "\n", createVNode(_components.pre, {
+      className: "astro-code",
+      style: {
+        backgroundColor: "#0d1117",
+        overflowX: "auto"
+      },
+      children: createVNode(_components.code, {
+        children: [createVNode(_components.span, {
+          className: "line",
+          children: createVNode(_components.span, {
+            style: {
+              color: "#c9d1d9"
+            },
+            children: "[A] / [\u2190]: Move Left"
+          })
+        }), "\n", createVNode(_components.span, {
+          className: "line",
+          children: createVNode(_components.span, {
+            style: {
+              color: "#c9d1d9"
+            },
+            children: "[D] / [\u2192]: Move Right"
+          })
+        }), "\n", createVNode(_components.span, {
+          className: "line",
+          children: createVNode(_components.span, {
+            style: {
+              color: "#c9d1d9"
+            },
+            children: "[Space] / [\u2191]: Jump"
+          })
+        }), "\n", createVNode(_components.span, {
+          className: "line",
+          children: createVNode(_components.span, {
+            style: {
+              color: "#c9d1d9"
+            },
+            children: "[P]: Restart level"
+          })
+        }), "\n", createVNode(_components.span, {
+          className: "line",
+          children: createVNode(_components.span, {
+            style: {
+              color: "#c9d1d9"
+            },
+            children: "[Escape]: Exit game"
+          })
+        })]
+      })
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {
+    wrapper: MDXLayout
+  } = props.components || {};
+  return MDXLayout ? createVNode(MDXLayout, {
+    ...props,
+    children: createVNode(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+__astro_tag_component__(getHeadings, "astro:jsx");
+__astro_tag_component__(MDXContent, "astro:jsx");
+const url = "src/content/posts/divided-chains-combined-brains.mdx";
+const file = "/home/runner/work/website/website/src/content/posts/divided-chains-combined-brains.mdx";
+const Content = (props = {}) => MDXContent({
+											...props,
+											components: { Fragment, ...props.components },
+										});
+Content[Symbol.for('astro.needsHeadRendering')] = !Boolean(frontmatter.layout);
+
+export { Content, Content as default, file, frontmatter, getHeadings, url };
