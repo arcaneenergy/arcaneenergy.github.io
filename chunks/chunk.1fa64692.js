63a77b23b162b2192ccd1787d1c9a5dbe5734782
arcaneenergy <52855634+arcaneenergy@users.noreply.github.com>
2023-02-11 19:16:36 +0000

    Update

--- chunks/chunk.1fa64692.js
+++ chunks/chunk.1fa64692.js
@@ -2,7 +2,7 @@ import { _ as __astro_tag_component__ } from './chunk.37b49969.js';
 import 'html-escaper';
 
 async function getMod() {
-  return import('./chunk.ee8908db.js');
+  return import('./chunk.dda60370.js');
 }
 const collectedLinks = ["assets/asset.a8fa5c64.css","assets/asset.b0e483ef.css","assets/asset.733765c9.css"];
 const collectedStyles = "@@ASTRO-STYLES@@";
