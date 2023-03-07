095eba63820538d8a50f1ccabaee76b11d7b3557
arcaneenergy <52855634+arcaneenergy@users.noreply.github.com>
2023-03-07 12:46:58 +0000

    Update

--- chunks/chunk.fc19545d.js
+++ chunks/chunk.fc19545d.js
@@ -784,7 +784,7 @@ const $$page = createComponent(async ($$result, $$props, $$slots) => {
 			</svg>
 			<span class="astro-6PAWVVAF">Articles & Tutorials</span>
 		</a>
-		<a href="/tags/game" class="astro-6PAWVVAF">
+		<a href="/tags/software" class="astro-6PAWVVAF">
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="astro-6PAWVVAF">
 				<path stroke="none" d="M0 0h24v24H0z" fill="none" class="astro-6PAWVVAF"></path>
 				<path d="M12 5h3.5a5 5 0 0 1 0 10h-5.5l-4.015 4.227a2.3 2.3 0 0 1 -3.923 -2.035l1.634 -8.173a5 5 0 0 1 4.904 -4.019h3.4z" class="astro-6PAWVVAF"></path>
