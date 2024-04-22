5b67d7a4cbfcb1cee78a5dda0cf8dab02f896435
arcaneenergy <52855634+arcaneenergy@users.noreply.github.com>
2024-04-22 19:23:54 +0000

    Update

---
+++ chunks/chunk.f616df99.js
@@ -0,0 +1,15 @@
+import { _ as __astro_tag_component__ } from './chunk.88f5223f.js';
+import 'module';
+import 'node:fs/promises';
+import 'node:url';
+import 'html-escaper';
+
+async function getMod() {
+  return import('./chunk.936e9c08.js');
+}
+const collectedLinks = ["/assets/asset.d01e868e.css","/assets/asset.6aeb2496.css","/assets/asset.733765c9.css"];
+const collectedStyles = "@@ASTRO-STYLES@@";
+const collectedScripts = "@@ASTRO-SCRIPTS@@";
+__astro_tag_component__(getMod, "astro:jsx");
+
+export { collectedLinks, collectedScripts, collectedStyles, getMod };
